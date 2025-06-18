aa891a4b899aea4f470fa20078570b4b94348fce
Mohamed Sabry <Mohamed.sabry.av@gmail.com>
2025-06-18 09:37:58 +0300

    Fix fetch issues: correct wcStoreApiUrl in prod environment and fix wp-json proxy config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,6 @@ export const environment = {
     wordpressApiUrl: 'https://adventures-hub.com/wp-json/wc/v3/',
     customApiUrl: '/api/custom',
     authApiUrl: 'https://adventures-hub.com/wp-json/jwt-auth/v1',
-    wcStoreApiUrl: 'https://adventures-hub.com:/wp-json/wc/store/v1',
+    wcStoreApiUrl: 'https://adventures-hub.com/wp-json/wc/store/v1',
     
   };
